f65250fc694fe0431e5296b22c2720a878980df7
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-09 20:37:19 +0200

    ms testing

---
+++ CG.Testing.MyLibraryMSTest/OperationMSTest.cs
@@ -0,0 +1,21 @@
+﻿namespace CG.Testing.MyLibrary.MSTest
+{
+    [TestClass]
+    public class OperationMSTest
+    {
+        [TestMethod]
+        public void AddNumbers_InputTwoNumbers_GetCorrectValue()
+        {
+            // Arrange
+            Operation op = new Operation();
+            int numTest1 = 20;
+            int numTest2 = 30;
+
+            // Act
+            int result = op.AddNumbers(numTest1, numTest2);
+
+            // Asser
+            Assert.AreEqual(50, result);
+        }
+    }
+}
