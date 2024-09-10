c82b1bd8068a3afa95941d5cfa17f3f2658d4978
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-10 19:16:10 +0200

    operation method testing

---
+++ CG.Testing.MyLibraryNUnitTest/OperationNUnitTest.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using NUnit.Framework.Legacy;
+
+namespace CG.Testing.MyLibrary
+{
+    [TestFixture]
+    public class OperationNUnitTest
+    {
+        [Test]
+        public void AddNumbers_InputTwoNumbers_GetCorrectValue()
+        {
+            // Arrange
+            Operation op = new Operation();
+            int numTest1 = 20;
+            int numTest2 = 30;
+
+            // Act
+            int result = op.AddNumbers(numTest1, numTest2);
+
+            // Assert
+            ClassicAssert.AreEqual(50, result);
+
+            //// Assert (usando la sintaxis moderna)
+            //Assert.That(result, Is.EqualTo(50));
+        }
+    }
+}
