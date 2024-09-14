e13ec01ba9f7a1c31d8952e14c974dadcf85b8c8
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-14 16:08:04 +0200

    mocking implementation

--- CG.Testing.MyLibraryNUnitTest/BankAccountNUnitTest.cs
+++ CG.Testing.MyLibraryNUnitTest/BankAccountNUnitTest.cs
@@ -1,22 +1,40 @@
-﻿using NUnit.Framework;
+﻿using Moq;
+using NUnit.Framework;
 
 namespace CG.Testing.MyLibrary
 {
     [TestFixture]
     public class BankAccountNUnitTest
     {
-        private BankAccount bankAccount;
+        private BankAccount account;
 
         [SetUp]
         public void SetUp()
         {
-            bankAccount = new BankAccount(new LoggerFake());
         }
 
         [Test]
-        public void Deposit_InputAmount100_ReturnsTrue()
+        public void Deposit_InputAmount100LoggerFake_ReturnsTrue()
         {
             // Arrange
+            BankAccount bankAccount = new BankAccount(new LoggerFake());
+            var amount = 100;
+
+            // Act
+            var result = bankAccount.Deposit(amount);
+
+            // Assert
+            Assert.That(result, Is.True);
+            Assert.That(bankAccount.Balance, Is.EqualTo(100));
+        }
+
+
+        [Test]
+        public void Deposit_InputAmount100Mocking_ReturnsTrue()
+        {
+            // Arrange
+            var loggerMock = new Mock<ILoggerGeneral>();
+            BankAccount bankAccount = new BankAccount(loggerMock.Object);
             var amount = 100;
 
             // Act
