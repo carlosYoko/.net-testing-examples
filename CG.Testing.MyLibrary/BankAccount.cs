b88fbb010c76882bea0f20fe1b79f4f2229bd9c3
Carlos Gimenez <carlosgibe@gmail.com>
2024-09-14 15:32:07 +0200

    create bankaccount class

---
+++ CG.Testing.MyLibrary/BankAccount.cs
@@ -0,0 +1,35 @@
+﻿namespace CG.Testing.MyLibrary
+{
+    public class BankAccount
+    {
+        public int Balance { get; set; }
+
+        public BankAccount()
+        {
+            Balance = 0;
+        }
+
+        public bool Deposit(int amount)
+        {
+            Balance += amount;
+            return true;
+        }
+
+        public bool Retire(int amount)
+        {
+            if (amount < Balance)
+            {
+                Balance -= amount;
+                return true;
+            };
+
+            return false;
+        }
+
+        public int GetBalance()
+        {
+            return Balance;
+        }
+
+    }
+}
